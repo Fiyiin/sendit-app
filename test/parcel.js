e6028e941425ace8df75550a4a4224b42af325cc
Fiyin <kolawolemmanuel@hotmail.com>
2018-11-09 19:24:43 +0100

    Tests for get all parcels

--- test/parcel.js
+++ test/parcel.js
@@ -1,11 +1,15 @@
-import request from 'supertest';
+import supertest from 'supertest';
+import { expect } from 'chai';
+import uuid from 'uuid';
+
 import app from '../app';
 
 describe('#POST /parcels', () => {
   it('creates a new parcel order', (done) => {
-    request(app)
+    supertest(app)
       .post('/api/v1/parcels')
       .send({
+        id: uuid.v4(),
         name: 'Brown Box',
         description: 'Leave me alone',
         pickup: 'Sabo',
@@ -13,9 +17,39 @@ describe('#POST /parcels', () => {
         username: 'Isamami',
         weight: '3g',
       })
+      .send({
+        id: uuid.v4(),
+        name: 'Brown Box',
+        description: 'Handle with care',
+        pickup: 'Ikeja',
+        delivery: 'Yaba',
+        username: 'Ipali',
+        weight: '4kg',
+      })
+      .send({
+        id: uuid.v4(),
+        name: 'Brown Box',
+        description: 'Handle with care',
+        pickup: 'Ikeja',
+        delivery: 'Yaba',
+        username: 'Ipali',
+        weight: '4kg',
+      })
       .expect(201)
       .expect('Content-Type', /application\/json/)
       .expect({ message: 'new parcel order' })
       .end(done);
+  });
+});
+
+describe('#GET /parcels', () => {
+  it('returns all parcels', (done) => {
+    supertest(app)
+      .get('/api/v1/parcels')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.an('arrray');
+        done();
+      });
   });
 });
