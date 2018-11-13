1c357d0d5345df73fda849fbe579f4d9c269b20b
Fiyin <kolawolemmanuel@hotmail.com>
2018-11-13 22:52:55 +0100

    Change project structure

--- build/app.js
+++ build/app.js
@@ -16,21 +16,22 @@ var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
 
-var _parcel = require('./api_v1/controllers/parcel');
+var _parcel = require('./controllers/parcel');
 
 var _parcel2 = _interopRequireDefault(_parcel);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+var port = process.env.PORT || 3000;
 
 app.use((0, _morgan2.default)('dev'));
 app.use(_express2.default.json());
 
-app.use(_express2.default.static(_path2.default.join(__dirname, './UI/')));
+app.use(_express2.default.static(_path2.default.join(__dirname, 'UI')));
 
 app.get('/', function (req, res) {
-  res.sendFile(_path2.default.join(__dirname, './ui/public/index.html'));
+  res.sendFile(_path2.default.join(__dirname, 'UI', 'public', 'index.html'));
 });
 
 app.post('/api/v1/parcels', _parcel2.default.create);
@@ -38,7 +39,8 @@ app.get('/api/v1/parcels', _parcel2.default.getAll);
 app.get('/api/v1/parcel/:id', _parcel2.default.getOne);
 app.put('/api/v1/parcel/:id', _parcel2.default.cancel);
 
-app.listen(3000);
-console.log('app running on port', 3000);
+app.listen(port, function () {
+  return console.log('app running on ' + port);
+});
 
 exports.default = app;
