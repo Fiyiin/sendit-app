7ae69525efa4a74c77ead612ba5092d432e47637
Fiyin <kolawolemmanuel@hotmail.com>
2018-11-21 09:31:03 +0100

    Create user signup endpoint

--- lib/withDB/sql/parcels.sql
+++ lib/withDB/sql/parcels.sql
@@ -1,10 +1,9 @@
-DROP DATABASE IF EXISTS sendit_testdb;
-CREATE DATABASE sendit_testdb;
 
 \c sendit_testdb;
 
 CREATE TABLE parcels (
-  ID SERIAL PRIMARY KEY,
+  id UUID PRIMARY KEY,
+  user_id UUUID NOT NULL
   name VARCHAR(140) NOT NULL,
   description TEXT,
   created_on TIMESTAMP DEFAULT LOCALTIMESTAMP(1),
@@ -13,12 +12,8 @@ CREATE TABLE parcels (
   weight NUMERIC NOT NULL,
   receivers_name TEXT,
   receivers_no TEXT,
-  status TEXT
+  status TEXT,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
-
-INSERT INTO parcels(name, description, pickup, delivery, weight,
-  receivers_name, receivers_no, status)
-VALUES ( 'Micheal Kors', 'Sunglasses', 'Yaba', 'Anthony',
-4, 'Teslim', '099972293', 'processing')
 
 RETURNING *;
